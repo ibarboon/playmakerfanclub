1cce34b27ed23f1e30401f841a0e1e89774b821f
Wiboon-iMac <Wiboon-iMac@Wiboons-iMac.local>
2014-09-29 22:49:01 +0700

    edit home view

---
+++ application/views/frontend/home_view.php
@@ -0,0 +1,63 @@
+<section id="superSizedSlider" class="scrollMenu">
+	<a id="prevslide" class="load-item"></a>
+	<a id="nextslide" class="load-item"></a>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div id="slidecaption"></div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="content">
+	<section class="color0">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 pt40" data-nekoanim="fadeInLeftBig" data-nekodelay="200">
+					<img src="<?php echo base_url('assets/frontend/images/theme-pics/flat-mobile.png'); ?>" alt="Mobile first by design" class="img-responsive" />
+			</div>
+			<div class="col-md-5 col-md-offset-1 pt40 mt40">
+				<h1>Mobile first by design</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="color1">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 pt40 mt40">
+				<h1>Easy to use</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+			<div class="col-md-6 col-md-offset-1 pt40 pb40" data-nekoanim="fadeInRightBig" data-nekodelay="300">
+				<img src="<?php echo base_url('assets/frontend/images/theme-pics/flat-laptop.png'); ?>" alt="Easy to use" class="img-responsive"/>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="projectQuoteSection" class="pt40 pb40 color2">
+	<div class="container">
+		<div class="row"> 
+			<div class="col-md-12 mb30 text-center" data-nekoanim="fadeInDown" data-nekodelay="300">
+				<h1>Hideo is the template you need for your next project</h1>
+				<p>
+					Spread the word :)
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
